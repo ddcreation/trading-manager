662c01429a4fa0f038a471b9c7b9b81e7e390ddb
David Daupeyroux <david.ddcreation@gmail.com>
2021-01-23 18:47:58 +0100

    Remove logs

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,5 @@
 const { REACT_APP_API_URL } = process.env;
 
-console.log(process.env, REACT_APP_API_URL);
-
 export const ApiResources = {
   cryptos: `${REACT_APP_API_URL}/cryptos`,
   healthcheck: `${REACT_APP_API_URL}/healthcheck`,
