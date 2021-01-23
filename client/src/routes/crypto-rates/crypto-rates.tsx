218621cb8d46f3ba0c7c7e4a03fe33d3cd2ee618
David Daupeyroux <david.ddcreation@gmail.com>
2021-01-23 16:19:02 +0100

    Connect to websocket test

--- client/src/routes/crypto-rates/crypto-rates.tsx
+++ client/src/routes/crypto-rates/crypto-rates.tsx
@@ -1,6 +1,36 @@
 import React from 'react';
 
 class CryptoRates extends React.Component {
+  ws = new WebSocket('ws://localhost:8080/');
+
+  componentDidMount() {
+    this.loadCryptos();
+  }
+
+  loadCryptos() {
+    //TODO: get resources from API
+
+    this.socketHandler();
+  }
+
+  socketHandler() {
+    this.ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log('connected');
+    };
+
+    this.ws.onmessage = (evt) => {
+      // listen to data sent from the websocket server
+      const message = JSON.parse(evt.data);
+      this.setState({ dataFromServer: message });
+      console.log(message);
+    };
+
+    this.ws.onclose = () => {
+      console.log('disconnected');
+    };
+  }
+
   render() {
     return (
       <div>
