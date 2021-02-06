28e154bb69455df623431e9a953cd7e4a1bfcfd9
David Daupeyroux <david.ddcreation@gmail.com>
2021-02-06 11:13:17 +0100

    Simulations API request

--- client/src/common/components/simulations-group/SimulationsGroup.tsx
+++ client/src/common/components/simulations-group/SimulationsGroup.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Alert } from 'react-bootstrap';
+import api from '../../../utils/api';
+import TmLoader from '../tm-loader/TmLoader';
 
 interface SimulationsGroupProps {
   symbol: string;
@@ -6,16 +9,54 @@ interface SimulationsGroupProps {
 
 interface SimulationsGroupState {
   loading: boolean;
+  simulations: unknown[];
 }
 
 class SimulationsGroup extends React.Component<
   SimulationsGroupProps,
   SimulationsGroupState
 > {
-  state = { loading: true };
+  state = { loading: true, simulations: [] };
+
+  componentDidMount() {
+    this.loadSimulations();
+  }
+
+  componentDidUpdate(prevProps: SimulationsGroupProps) {
+    if (prevProps.symbol !== this.props.symbol) {
+      this.loadSimulations();
+    }
+  }
+
+  loadSimulations() {
+    this.setState({ loading: true, simulations: [] });
+    api
+      .retrieve<unknown[]>(
+        `${api.resources.cryptos}/${this.props.symbol}/simulations`
+      )
+      .then(
+        (response) => {
+          this.setState({ loading: false, simulations: response });
+        },
+        (error) => {
+          this.setState({ loading: false, simulations: [] });
+        }
+      );
+  }
 
   render() {
-    return <div>SimulationsGroup X</div>;
+    return (
+      <div>
+        {!this.state.loading &&
+        this.state.simulations &&
+        this.state.simulations.length ? (
+          <div>SimulationsGroup X</div>
+        ) : (
+          <Alert variant='danger'>ERROR: Unable to retrieve simulations.</Alert>
+        )}
+        {this.state.loading && <TmLoader />}
+      </div>
+    );
   }
 }
 
