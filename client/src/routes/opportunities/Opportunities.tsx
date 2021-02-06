bcff9af8deb053a4db5113575561329dbf8cd641
David Daupeyroux <david.ddcreation@gmail.com>
2021-02-06 11:13:44 +0100

    Removed unused import

--- client/src/routes/opportunities/Opportunities.tsx
+++ client/src/routes/opportunities/Opportunities.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGroup, Row } from 'react-bootstrap';
+import { ListGroup } from 'react-bootstrap';
 import { CryptoCard, SimulationsGroup } from '../../common/components';
 import api from '../../utils/api';
 
