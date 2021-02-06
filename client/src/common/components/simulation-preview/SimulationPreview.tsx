1fcca44788d2735388ccfdbddc6993b07e187189
David Daupeyroux <david.ddcreation@gmail.com>
2021-02-06 22:11:42 +0100

    Simulation trades array

--- client/src/common/components/simulation-preview/SimulationPreview.tsx
+++ client/src/common/components/simulation-preview/SimulationPreview.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import { Alert, Card, Col, Container, Row } from 'react-bootstrap';
+import { Alert, Card, Col, Container, Row, Table } from 'react-bootstrap';
 import TmLoader from '../tm-loader/TmLoader';
 import api from '../../../utils/api';
 import PercentBadge from '../percent-badge/PercentBadge';
+
+interface SimulationTrade {
+  entryPrice: number;
+  entryTime: string;
+  exitPrice: number;
+  exitTime: string;
+  holdingPeriod: number;
+  profit: number;
+}
+
+interface Simulation {
+  history: any[];
+  id: string;
+  name: string;
+  trades: SimulationTrade[];
+}
 
 interface SimulationPreviewProps {
   symbol: string;
@@ -10,7 +26,7 @@ interface SimulationPreviewProps {
 
 interface SimulationPreviewState {
   loading: boolean;
-  simulations: any[];
+  simulations: Simulation[];
 }
 
 class SimulationPreview extends React.Component<
@@ -58,7 +74,9 @@ class SimulationPreview extends React.Component<
                       <Container className='px-0'>
                         <Row>
                           <Col md='auto'>
-                            <Card.Title>{simulation.name}</Card.Title>
+                            <Card.Title>
+                              <h4>{simulation.name}</h4>
+                            </Card.Title>
                           </Col>
                           <Col>
                             <div className='float-right'>
@@ -72,7 +90,55 @@ class SimulationPreview extends React.Component<
                       </Container>
                     </Card.Header>
                     <Card.Body>
-                      {JSON.stringify(this.state.simulations)}
+                      {simulation.trades.length && (
+                        <Table striped bordered size='sm'>
+                          <thead>
+                            <tr>
+                              <th>Entry time</th>
+                              <th>Entry price</th>
+                              <th>Exit time</th>
+                              <th>Exit price</th>
+                              <th>Duration</th>
+                              <th>Difference</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {simulation.trades.map((trade: any) => {
+                              let diffDaysVariant = 'success';
+                              if (
+                                10 < trade.holdingPeriod &&
+                                trade.holdingPeriod < 30
+                              ) {
+                                diffDaysVariant = 'warning';
+                              } else if (trade.holdingPeriod >= 30) {
+                                diffDaysVariant = 'danger';
+                              }
+
+                              return (
+                                <tr>
+                                  <td>{trade.entryTime.substr(0, 10)}</td>
+                                  <td>{trade.entryPrice}</td>
+                                  <td>{trade.exitTime.substr(0, 10)}</td>
+                                  <td>{trade.exitPrice}</td>
+                                  <td className={'table-' + diffDaysVariant}>
+                                    {trade.holdingPeriod}
+                                  </td>
+                                  <td
+                                    className={
+                                      trade.profit > 0
+                                        ? 'table-success'
+                                        : 'table-danger'
+                                    }
+                                  >
+                                    {(trade.profit > 0 ? '+' : '') +
+                                      trade.profit.toFixed(2)}
+                                  </td>
+                                </tr>
+                              );
+                            })}
+                          </tbody>
+                        </Table>
+                      )}
                     </Card.Body>
                   </Card>
                 )
