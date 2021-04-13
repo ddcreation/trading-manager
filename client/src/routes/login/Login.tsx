95393337f336ee78c009c8095e08c2b44204c0cc
David Daupeyroux <david.ddcreation@gmail.com>
2021-04-13 21:28:09 +0200

    Login page

--- client/src/routes/login/Login.tsx
+++ client/src/routes/login/Login.tsx
@@ -1,12 +1,32 @@
 import { Component } from 'react';
-import api from '../../utils/api';
+import { Button, Card, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class LoginRoute extends Component {
   render() {
     return (
-      <div>
-        <h1>Login</h1>
-      </div>
+      <Container>
+        <Card className='col-md-4 mx-auto mt-5 text-center'>
+          <Card.Body>
+            <Card.Title>Login</Card.Title>
+            <Form>
+              <Form.Group controlId='username'>
+                <Form.Control type='text' placeholder='Username' />
+              </Form.Group>
+
+              <Form.Group controlId='password'>
+                <Form.Control type='password' placeholder='Password' />
+              </Form.Group>
+
+              <Button className='w-100'>Login</Button>
+
+              <Button as={Link} to='/register' variant='link' className='w-100'>
+                Register
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Container>
     );
   }
 }
