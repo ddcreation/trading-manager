3d085b9a6a9c19efd8a652711c5bc759a55aae71
David Daupeyroux <david.ddcreation@gmail.com>
2021-04-13 22:54:23 +0200

    Login form validations

--- client/src/routes/login/Login.tsx
+++ client/src/routes/login/Login.tsx
@@ -2,20 +2,61 @@ import { Component } from 'react';
 import { Button, Card, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-class LoginRoute extends Component {
+interface LoginRouteState {
+  form: {
+    [k: string]: string;
+  };
+  errors: {
+    [k: string]: string;
+  };
+}
+
+class LoginRoute extends Component<unknown, LoginRouteState> {
+  constructor(props: unknown) {
+    super(props);
+
+    this.state = {
+      form: {},
+      errors: {},
+    };
+  }
+
+  updateField(name: string, value: string) {
+    this.setState({
+      form: { ...this.state.form, [name]: value },
+    });
+  }
+
   render() {
     return (
       <Container>
         <Card className='col-md-4 mx-auto mt-5 text-center'>
           <Card.Body>
             <Card.Title>Login</Card.Title>
-            <Form>
+            <Form noValidate onSubmit={this.submitForm}>
               <Form.Group controlId='username'>
-                <Form.Control type='text' placeholder='Username' />
+                <Form.Control
+                  type='text'
+                  placeholder='Username'
+                  onChange={(e) => this.updateField('username', e.target.value)}
+                  isInvalid={!!this.state.errors.username}
+                />
+                <Form.Control.Feedback type='invalid'>
+                  {this.state.errors.username}
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Form.Group controlId='password'>
-                <Form.Control type='password' placeholder='Password' />
+                <Form.Control
+                  type='password'
+                  name='password'
+                  placeholder='Password'
+                  onChange={(e) => this.updateField('password', e.target.value)}
+                  isInvalid={!!this.state.errors.password}
+                />
+                <Form.Control.Feedback type='invalid'>
+                  {this.state.errors.password}
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Button className='w-100' type='submit'>
@@ -31,6 +72,33 @@ class LoginRoute extends Component {
       </Container>
     );
   }
+
+  public submitForm = (event: any) => {
+    event.preventDefault();
+
+    const errors: { [k: string]: string } = {};
+    ['username', 'password'].forEach((field) => {
+      if (!this.state.form[field] || this.state.form[field].length === 0) {
+        errors[field] = 'Required';
+      }
+    });
+
+    this.setState(
+      {
+        errors,
+      },
+      () => {
+        if (
+          Object.keys(this.state.errors).filter(
+            (key: string) => this.state.errors[key]
+          ).length === 0
+        ) {
+          // TODO: call API login
+          console.log('submit form', this.state);
+        }
+      }
+    );
+  };
 }
 
 export default LoginRoute;
