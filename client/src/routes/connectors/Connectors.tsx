cffd67d2e7adce12cd3e3bb17414c05262e7e2c3
David Daupeyroux <david.ddcreation@gmail.com>
2021-04-18 00:15:15 +0200

    Prepare for submission

--- client/src/routes/connectors/Connectors.tsx
+++ client/src/routes/connectors/Connectors.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Form } from 'react-bootstrap';
+import { Accordion, Button, Card, Form } from 'react-bootstrap';
 import { TmLoader } from '../../common/components';
 import { ConnectorConfig } from '../../common/models/Connector';
 import { connectorsService } from '../../services/connectors.service';
@@ -33,6 +33,7 @@ class ConnectorsRoute extends React.Component<unknown, ConnectorsRouteState> {
       return { ...acc, [property]: value };
     }, {});
 
+    form.connector_id = connectorId;
     form.enable = formElements.enable.checked;
 
     console.log(form, connectorId);
@@ -41,39 +42,55 @@ class ConnectorsRoute extends React.Component<unknown, ConnectorsRouteState> {
   render() {
     return this.state ? (
       this.state.connectors.map((connector) => (
-        <Form
-          key={connector.id}
-          onSubmit={(e) => this.submitConnectorConfig(e, connector.id)}
-        >
+        <Accordion key={connector.id} defaultActiveKey='0'>
           <Card className='my-5'>
             <Card.Header>
               <Card.Title>
                 <h2>{connector.name}</h2>
               </Card.Title>
             </Card.Header>
-            <Card.Body>
-              {Object.keys(connector.properties).map((property) => (
-                <Form.Group
-                  key={`formConnector${connector.id}.${property}`}
-                  controlId={`formConnector${connector.id}.${property}`}
+            <Accordion.Toggle as={Card.Header} eventKey='0'>
+              Connector parameters
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey='0'>
+              <Card.Body>
+                <Form
+                  onSubmit={(e) => this.submitConnectorConfig(e, connector.id)}
                 >
-                  {this.renderConnectorProperty(connector, property)}
-                </Form.Group>
-              ))}
-              <Form.Group controlId={`formConnector${connector.id}.enable`}>
-                <Form.Check
-                  type='checkbox'
-                  name='enable'
-                  label='Activate connector'
-                  value='1'
-                />
-              </Form.Group>
-              <Button variant='primary' type='submit'>
-                Save
-              </Button>
-            </Card.Body>
+                  {Object.keys(connector.properties).map((property) => (
+                    <Form.Group
+                      key={`formConnector${connector.id}.${property}`}
+                      controlId={`formConnector${connector.id}.${property}`}
+                    >
+                      {this.renderConnectorProperty(connector, property)}
+                    </Form.Group>
+                  ))}
+                  <Form.Group controlId={`formConnector${connector.id}.enable`}>
+                    <Form.Check
+                      type='checkbox'
+                      name='enable'
+                      label='Activate connector'
+                    />
+                  </Form.Group>
+                  <Button variant='primary' type='submit'>
+                    Save
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Accordion.Collapse>
+            <Accordion.Toggle as={Card.Header} eventKey='1' disabled>
+              Favorites symbols
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey='1'>
+              <Card.Body>
+                TODO: List symbols
+                <Button variant='primary' type='submit'>
+                  Save
+                </Button>
+              </Card.Body>
+            </Accordion.Collapse>
           </Card>
-        </Form>
+        </Accordion>
       ))
     ) : (
       <TmLoader />
