cd8352d7a638c7322ab8c3bf377e7df266da9423
David Daupeyroux <david.ddcreation@gmail.com>
2021-05-30 22:12:18 +0200

    Debug front error notification

--- client/src/services/axios/interceptors/error-notification.ts
+++ client/src/services/axios/interceptors/error-notification.ts
@@ -5,17 +5,18 @@ import {
 import { notifyAction, store } from '../../../redux';
 
 export const errorNotificationInterceptor = (error: any): Promise<never> => {
+  console.log(error);
   store.dispatch(
     notifyAction({
       type: NotificationType.ERROR,
       title:
-        error?.response?.data.code ||
+        error?.data.code ||
         `HTTP Error ${error.status || 'server not responding'}`,
-      body: error?.response?.data.message || error.statusText,
+      body: error?.data.message || error.statusText,
       persistent: false,
       dismissable: true,
     } as NotificationConfig)
   );
 
-  return Promise.reject(error.response);
+  return Promise.reject(error);
 };
